15e8912ff243829cbc44f559d63195b61741c22a
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-05-24 18:13:44 +0300

    passing

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, pages):
+        if isinstance(pages, int):
+            self._page_count = pages
+        else:
+            print("page_count must be an integer")
     
-        
+    # @turn_page.setter
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
